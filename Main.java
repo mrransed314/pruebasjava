6eb281ff880743dfabad012e10cc9b58080a4cad
andres moreno <servicesmoan@gmail.com>
2025-04-18 07:15:52 -0500

    nuevo cambio sout 2

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main{
     public static void main(String[] args){
 
         System.out.println("funciona java prueba 2");
+        System.out.println("prueba 2");
         System.out.println("La conexion con git hub se ha hecho");
 
     }
