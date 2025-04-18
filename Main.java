d9bc5c04069f16a203e4ec8ab3a9ae83ba378a9b
andres moreno <servicesmoan@gmail.com>
2025-04-17 22:35:33 -0500

    nuevo cambio

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main{
     public static void main(String[] args){
 
         System.out.println("funciona java");
+        System.out.println("La conexion con git hub se ha hecho");
 
     }
 
