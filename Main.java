f29ee75f4e3a13e696c445e45fd8a6998f5b66f3
andres moreno <servicesmoan@gmail.com>
2025-04-18 08:14:58 -0500

    cambios realizados 6

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main{
     public static void main(String[] args){
 
         System.out.println("funciona java prueba 2");
-        System.out.println("prueba 5");
+        System.out.println("prueba 6");
         System.out.println("La conexion con git hub se ha hecho");
 
     }
